62fa8f6e56c78435ea7d7f209235481ace152eb8
Da-Colon <dcolonantonio@gmail.com>
2019-08-25 21:27:14 -0400

    Javascript for LP works now

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,4 @@
-function displayNextSlide() {
-    x = (x === images.length - 1) ? 0 : x + 1;
-    document.getElementById("slides").src = images[x];
-}
-
-function displayPrevSlide() {
-    x = (x === images.length - 1) ? 0 : Math.abs(x - 1);
-    document.getElementById("slides").src = images[x];
-}
-
-var images = [], x = -1;
+let images = [], x = -1;
 images[0] = "images/0-vs-code.png";
 images[1] = "images/html.png";
 images[2] = "images/css.png";
@@ -16,13 +6,73 @@ images[3] = "images/javascript.png";
 images[4] = "images/python.png";
 images[5] = "images/react.png";
 
+function displayNextSlide() {
+    x = (x === images.length - 1) ? 0 : x + 1;
+    document.getElementById("slides").src = images[x];
+    changeInfo(images[x]);
+    changeLink(images[x]);
+}
+
+function displayPrevSlide() {
+    x = (x === 0) ? images.length -1  : x - 1;
+    document.getElementById("slides").src = images[x];
+    changeInfo(images[x]);
+    changeLink(images[x]);
+}
 
 
-function changeinfo(displayNextSlide, displayPrevSlide){
-    let infoTitle = document.getElementById("info-title");
-    switch(document.getElementById("slides").src){
-        case "images/0-vs-ConvolverNode.png":
+
+
+function changeInfo(x){
+        let infoTitle = document.getElementById("info-title");
+        switch(x){
+            case images[0]:
             infoTitle.innerHTML = "VS Code";
             break;
-    }
+            case images[1]:
+            infoTitle.innerHTML = "HTML";
+            break;
+            case images[2]:
+            infoTitle.innerHTML = "CSS";
+            break;
+            case images[3]:
+            infoTitle.innerHTML = "Javascript";
+            break;
+            case images[4]:
+            infoTitle.innerHTML = "Python";
+            break;
+            case images[5]:
+            infoTitle.innerHTML = "React.JS";
+            break;
+        }
 }
+function changeLink(x){
+        let infoLinks = document.getElementById("infoLinks");
+        switch(x){
+            case images[0]:
+            infoLinks.href = "techPages/vscode.html";
+            break;
+            case images[1]:
+            infoLinks.href = "techPages/html.html";
+            break;
+            case images[2]:
+            infoLinks.href = "techPages/css.html";
+            break;
+            case images[3]:
+            infoLinks.href = "techPages/javasript.html";
+            break;
+            case images[4]:
+            infoLinks.href = "techPages/python.html";
+            break;
+            case images[5]:
+            infoLinks.href = "techPages/react.html";
+            break;
+        }
+}
+// Change Info as an if/else statement
+// function changeInfo(x){
+//     let infoTitle = document.getElementById("info-title");
+//     if (x === images[0]) {
+//         infoTitle.innerHTML = "VS Code";
+//     }
+// }
